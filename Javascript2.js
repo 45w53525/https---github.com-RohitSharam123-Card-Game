a3597cb92fcf020b3adf11d89772b3ced8157758
Rohit Sharma <pc@DESKTOP-H94GUCO>
2022-08-20 18:05:52 -0400

    first commit

---
+++ Javascript2.js
@@ -0,0 +1,76 @@
+window.onload = pageload;
+
+function pageload() {
+
+    // var button = document.getElementById("btn");
+   
+
+
+    $('#one1').click(function() {
+
+
+        $(this).toggleClass('Rotatecard');
+
+    });
+
+    $('#one2').click(function() {
+
+
+        $(this).toggleClass('Rotatecard');
+
+    });
+    $('#one3').click(function() {
+
+
+        $(this).toggleClass('Rotatecard');
+
+    });
+    $('#one4').click(function() {
+
+
+        $(this).toggleClass('Rotatecard');
+
+    });
+    $('#one5').click(function() {
+
+
+        $(this).toggleClass('Rotatecard');
+
+    });
+    $('#one6').click(function() {
+
+
+        $(this).toggleClass('Rotatecard');
+
+    });
+    $('#one7').click(function() {
+
+
+        $(this).toggleClass('Rotatecard');
+
+    });
+    
+    $('#one8').click(function() {
+
+
+        $(this).toggleClass('Rotatecard');
+
+    });$('#one9').click(function() {
+
+
+        $(this).toggleClass('Rotatecard');
+
+    }); $('#one10').click(function() {
+
+
+        $(this).toggleClass('Rotatecard');
+
+    });
+
+
+
+
+
+
+
+}
